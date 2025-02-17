50e52ef364e167868dceac4ebe201827689b7f77
yehormiaus <myaus.egor@gmail.com>
2025-02-17 19:40:36 +0000

    add add main.tf file

---
+++ terraform/main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.81.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = var.aws_region
+  profile = var.aws_profile
+}
+
+data "aws_organizations_organization" "org" {}
+
+module "ec2" {
+  source        = "../modules/ec2"
+  security_group_ids = [module.security.sg_id]  # Get security group ID from module
+  key_pair_name = module.key_pair.key_pair_name  # Get key pair name from module
+  iam_instance_profile_name = module.iam.instance_profile_name # Attach IAM Profile
+  iam_role_name = module.iam.role_name  # Get IAM role name from module
+}
+
+module "iam" {
+  source = "../modules/iam"
+}
+
+module "key_pair" {
+  source = "../modules/key_pair"
+}
+
+module "secrets" {
+  source = "../modules/secrets"
+}
+
+module "security" {
+  source = "../modules/security"
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,3 @@
-variable "instance_type" {
-  description = "EC2 instance type"
-  type        = string
-  default     = "t2.micro"
-}
-
 variable "aws_region" {
   description = "AWS region to deploy resources"
   type        = string
@@ -14,36 +8,6 @@ variable "aws_profile" {
   description = "AWS profile from ~/.aws/credentials"
   type        = string
   default     = "admin-cli-user"
-}
-
-variable "ami_filter" {
-  description = "Filter to get latest Amazon Linux 2 AMI"
-  type = list(string)
-  default = ["amzn2-ami-hvm-*-x86_64-gp2"]
-}
-
-variable "allowed_ssh_ip" {
-  description = "Allowed SSH IP CIDR (modify for security)"
-  type = list(string)
-  default = ["94.63.118.183/32"]
-}
-
-variable "ec2_name" {
-  description = "EC2 instance name"
-  type        = string
-  default     = "my-ec2-instance"
-}
-
-variable "key_name" {
-  description = "Key pair name for SSH access"
-  type        = string
-  default     = "my-ec2-key"
-}
-
-variable "secret_name" {
-  description = "Name of the AWS Secrets Manager secret"
-  type        = string
-  default     = "my-test-secret"
 }
 
 
